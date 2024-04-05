c1c52bd1b3eff84d185940181eb45e28cdbe3b05
DanielHansensen <danielhansendaniel@icloud.com>
2024-04-05 09:38:33 +0200

    gjorde flere ting

---
+++ script/classes/abilityClass.js
@@ -0,0 +1,15 @@
+// ability class definition
+class Ability {
+    constructor(player, type, sybtype, parameters) {
+        switch (type) {
+            case 'AoE':
+                break;
+            case 'physical':
+                break;
+            case 'projectile':
+                break;
+            case 'cone':
+                break;
+        }
+    }
+}
