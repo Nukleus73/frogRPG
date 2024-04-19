5e678ee98a80140f68fb31fcce2970b88448b2c1
DanielHansensen <danielhansendaniel@icloud.com>
2024-04-19 08:57:36 +0200

    commit for pull av nyeste filer

--- newScript/formatMatrix.js
+++ newScript/formatMatrix.js
@@ -21,20 +21,28 @@ function formatMatrix(matrix) {
         y < matrix.length - 1 && x > 0 ? matrix[y + 1][x - 1] : undefined;
       let topLeft = y > 0 && x > 0 ? matrix[y - 1][x - 1] : undefined;
 
+      // If current cell is a left cell
       if (center == 11 && right == 0) {
         newMatrix[y][x] = 2;
       }
-      // If current cell is a top cell (1)
+      // If current cell is a right cell
       if (center == 11 && left == 0) {
         newMatrix[y][x] = 4;
       }
-      // if (center == 11 && top == 0) {
-      //   newMatrix[y][x] = 3;
-      // }
-      // // If current cell is a top cell (1)
-      // if (center == 11 && bottom == 0) {
-      //   newMatrix[y][x] = 1;
-      // }
+      // If current cell is a bottom cell
+      if (center == 11 && top == 0) {
+        newMatrix[y][x] = 3;
+      }
+      // If current cell is a top cell
+      if (center == 11 && bottom == 0) {
+        newMatrix[y][x] = 1;
+      }
+      // If current cell is a full cell
+      if (center == 11
+        && (bottom == 0 || top == 0)
+        && (left == 0 || right == 0)) {
+        newMatrix[y][x] = 10;
+      }
     }
   }
 
